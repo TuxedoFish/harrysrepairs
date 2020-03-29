8efb140fd277d372504dc72abd7a4bf657160e19
TuxedoFish <harryliversedge@gmail.com>
2020-03-29 21:02:28 +0100

    Added SSL certiicate

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+var sslRedirect = require('heroku-ssl-redirect');
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
+
+// enable ssl redirect
+app.use(sslRedirect());
+
 // the __dirname is the current directory from where the script is running
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
