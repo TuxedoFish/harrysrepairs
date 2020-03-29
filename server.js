a57f3ce5829496a91eaf77b19bfe6bfef94b8d42
TuxedoFish <harryliversedge@gmail.com>
2020-03-29 21:18:23 +0100

    Order seems to matter

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const sslRedirect = require('heroku-ssl-redirect');
+var sslRedirect = require('heroku-ssl-redirect');
 
 // the __dirname is the current directory from where the script is running
 app.use(favicon(__dirname + '/build/favicon.ico'));
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
 // enable ssl redirect
 app.use(sslRedirect())
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
