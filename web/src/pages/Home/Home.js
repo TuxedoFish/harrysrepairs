da32a53ad1be7c0567813de9d9e24215559b2f28
TuxedoFish <harryliversedge@gmail.com>
2021-02-07 13:09:09 +0000

    Fixed review containers widths

--- web/src/pages/Home/Home.js
+++ web/src/pages/Home/Home.js
@@ -30,13 +30,13 @@ const image_size = "150px"
 
 const reviews = [
   (
-    <EmbeddedPost width="170px" href="https://www.facebook.com/ben.wilson.3139/posts/4631873183553529"/>
+    <EmbeddedPost href="https://www.facebook.com/ben.wilson.3139/posts/4631873183553529"/>
   ),
   (
-    <EmbeddedPost width="170px" href="https://www.facebook.com/george.hill.79230305/posts/3422551254636553"/>
+    <EmbeddedPost href="https://www.facebook.com/george.hill.79230305/posts/3422551254636553"/>
   ),
   (
-    <EmbeddedPost width="170px" href="https://www.facebook.com/robert.warzee/posts/3506356199453170"/>
+    <EmbeddedPost href="https://www.facebook.com/robert.warzee/posts/3506356199453170"/>
   )
 ]
 
