d35c761e71874d0ae07d32677dee6344a2932110
Harry Liversedge <harryliversedge@gmail.com>
2021-06-05 10:30:18 +0100

    fix/webpack-config-bug

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -8,49 +8,58 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || 'N/A';
 const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID || 'sb';
 const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:1337';
+const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || 'N/A';
 
 module.exports = {
-  context: __dirname,
-
-  entry: {
-    index: './src/index.js',
-  },
-  output: {
-    publicPath: '/',
-    path: path.resolve(__dirname, `dist`),
-    filename: `[name].js`,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      favicon: './src/images/favicon.ico',
-    }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: "Harry's Repairs",
-      myPageHeader: 'Home Page',
-      template: './templates/index.html',
-      chunks: ['index'],
-      filename: './index.html', // relative to root of the application
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(NODE_ENV),
-        PAYPAL_CLIENT_ID: JSON.stringify(PAYPAL_CLIENT_ID),
-        REACT_APP_BACKEND_URL: JSON.stringify(REACT_APP_BACKEND_URL),
-        GOOGLE_ANALYTICS_ID: JSON.stringify(GOOGLE_ANALYTICS_ID)
-      },
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    context: __dirname,
+    
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        publicPath: '/',
+        path: path.resolve(__dirname, `dist`),
+        filename: `[name].js`,
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            favicon: "./src/images/favicon.ico",
+        }),
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: "Harry's Repairs",
+            myPageHeader: 'Home Page',
+            template: './templates/index.html',
+            chunks: ['index'],
+            filename: './index.html' // relative to root of the application
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(NODE_ENV),
+                'PAYPAL_CLIENT_ID': JSON.stringify(PAYPAL_CLIENT_ID),
+                'REACT_APP_BACKEND_URL': JSON.stringify(REACT_APP_BACKEND_URL),
+                'GOOGLE_ANALYTICS_ID': JSON.stringify(GOOGLE_ANALYTICS_ID)
+            }
+        }),
+    ],
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.(scss|css)$/,
+            use: [
+                { loader: `style-loader` },
+                { loader: `css-loader` },
+                { loader: `sass-loader` },
+            ],
         },
         {
             test: /\.(svg)$/,
